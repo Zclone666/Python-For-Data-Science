2c55d1be379bf70eb3fb4c22a0f724666e5dee7e
Свердлов Кирилл Анатольевичi <47571211+Zclone666@users.noreply.github.com>
2019-06-04 00:27:16 +0300

    Solution search task2 (#1)
    * Adapted for the second task
    
    * Adapted for the second task
    
    First part of the first task of the second task block
    
    Here's an example of how to create DataFrame correctly
    
    * Brand new DataFrame
    
    * new one
    
    * 5
    
    * 99
    
    * add
    
    * 0

---
+++ HW.py
@@ -0,0 +1,171 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  2 21:22:48 2019
+
+@author: cyril
+"""
+
+import numpy as ny
+import pandas as ps
+import datetime as dt
+import random as rand
+import math
+#import tables
+import textwrap
+
+
+"""
+
+    Задание №1
+
+
+"""
+
+authors=ps.DataFrame({'author_id':[1,2,3], 
+                      'author_name':['Тургенев','Чехов','Островский']}, columns=['author_id','author_name'])
+#_id', 'author_name']);
+print(authors.loc[1,:])
+
+
+#print(authors)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+
+        Задание №2
+#
+
+#
+
+#
+
+#
+
+#
+
+#
+
+#
+
+#
+
+#
+
+#
+
+#"""
+
+#
+
+#        Задание №2
+
+#
+
+#"""
+
+#
+
+#a_centred=a - ny.average(mean_a, axis=0)
+
+#zx_=ny.array([[a.mean(axis=0),a.mean(axis=1)]])
+
+#a_centred=ny.array([a[0] - mean_a_[0]])
+
+#cntrd = ny.cov((ny.transpose(a[0])))
+
+#{(vt - mean_a_[0+1*(vt%2)]) for vt in mean_a_ }
+
+#a_centred= a-zx_
+
+#print('a_centred = ', cntrd)
+
+#
+
+#print ('x = ', a[0][0], mean_a_[0])
+
+#
+
+#"""
+
+#
+
+#    Задание №3
+
+#
+
+#
+
+#"""
+
+#
+
+#a_centred_sp=a_centred.dot(a_centred)/(ny.size(a)-1)
+
+#
+
+#print('Fin (a_centred_sp) = ', a_centred_sp)
+
+#
+
+#print('Test; selftest: ', (a.transpose())[0][1])
+
+#ny.transpose(a)
+
+#
+
+#
+
+#
+
+#
+
+#
+
+#
+
+#
+
+#
+
+#
+
+#Задание №1
+
+#
+
+#
+
+#"""
+
+#
+
+#a_=ny.array([[1, 2, 3, 3, 1], [6, 8, 11, 10, 7]])
+
+#a_m=[1, 2, 3, 3, 1]
+
+#b_m=[6, 8, 11, 10, 7]
+
+#a=a_.transpose()
+
+#a=ny.dstack((a_m,b_m))
+
+#print
