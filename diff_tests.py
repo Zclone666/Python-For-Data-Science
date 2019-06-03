36b725865518639a4438885aad58a85d0edcc65a
Свердлов Кирилл Анатольевичi <47571211+Zclone666@users.noreply.github.com>
2019-06-03 09:06:58 +0300

    file with testing different technics

---
+++ diff_tests.py
@@ -0,0 +1,52 @@
+import numpy as n
+
+a_=[1, 2, 3, 3, 1]
+b_=[6, 8, 11, 10, 7]
+a=[[0,0]]
+l=0
+for i in a_:
+	u=a_.index(i);
+	print('i,a_[u] = ', i, a_[u])
+	if(u==0):
+		a[0][u]=[i,b_[u]]
+	elif (u%2==0):
+		l+=1
+#		print('u = ', u)
+		a[l].append([])
+	#u+=1
+	print('\na =', a)
+#	a[[a_[i],0]]=i
+"""	
+
+#a__=dict.
+
+a=n.array([[a_[0:,:, 2]]])
+#a=n.array([a_temp[0][0], a_temp[1][0]])
+for i in a_[i] and b_[i]:
+
+		if ()
+
+mean_a=[a.]
+a=[[None, None]]
+for x, y in ([a_[0:a_[x]:],b_[0:b_[y]:]]):
+for x, y in (a_, b_):
+		print(a_[x], x, y, a_)
+	if(a[0]==[None,None]):
+		a[0]=[x,y]
+	else:
+		a.append([x,y])
+	#for y in b_:
+#	a=a+[x,y]
+i=[[]]""
+#i[[]].append([a_[[]])#,b_[0::1]]
+print(a)
+
+#	a[0]=x[0::2]
+	#a[1]=x[1::2]
+#	print(a)
+	a=[x]
+	a.append(x)
+#	a=[[x[0:1], y[0:1]]]
+#a=n.array([a_,b_])
+print(a)
+#print(a_[0::2],b_[1::2])"""
