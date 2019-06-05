d6365699cbbd42ca221dad7dd57f72deb8f50f5d
Zclone666 <it.is.my.email.adress@gmail.com>
2019-06-05 21:58:16 +0300

    vs addition

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -29,20 +29,16 @@ books=ps.DataFrame({'author_id':[1, 1, 1, 2, 2, 3, 3],
  'price':[450, 300, 350, 500, 450, 370, 290]}, 
 columns=['author_id','book_title','price']);
 
-#print(authors)
-
-
 authors_price=ps.merge(authors, books, on='author_id', how='inner')
+
+auth_p=authors_price.drop(['author_id','book_title'], axis=1)
+print(auth_p)
 
 top5=authors_price.nlargest(5, 'price')
 
 print(top5)
 
 author_s=ps.DataFrame({}, columns=['author_name'])
-author_s=authors_price.groupby('author_name').agg(['min', 'max', 'mean'], axis=1, columns=['price'])       #{columns='price','min'})
+author_s=auth_p.groupby('author_name').agg(['min', 'max', 'mean'], axis=1, columns=['price'])       #{columns='price','min'})
 author_s.rename({'min':'min_price', 'max':'max_price', 'mean':'mean_price'}, axis='columns', inplace=True)
 print(author_s)
-#print(author_s.loc[author_s['price']<5])
-#author_s.drop(author_s.loc[author_s['min_price', 'max_price', 'mean_price']<5], axis=0)
-#author_s.rename({'price':'min_price'}, axis='columns', inplace=True)
-#print(author_s)
